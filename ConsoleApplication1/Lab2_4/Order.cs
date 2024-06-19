5c358f8f8a3372e666eee682a0f0d0b73c822ce2
sashamark <sashamark7777@gmail.com>
2024-06-20 00:31:11 +0300

    lab2_4 completed

---
+++ ConsoleApplication1/Lab2_4/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Lab2_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var orders = new List<Order>
+            {
+                new Order { OrderId = 1, OrderDate = DateTime.Today, Products = new List<Product>
+                    {
+                        new Product { ProductId = 1, Name = "Товар 1", Price = 100 },
+                        new Product { ProductId = 2, Name = "Товар 2", Price = 150 }
+                    }
+                },
+                new Order { OrderId = 2, OrderDate = DateTime.Today.AddDays(-1), Products = new List<Product>
+                    {
+                        new Product { ProductId = 3, Name = "Товар 3", Price = 75 },
+                        new Product { ProductId = 4, Name = "Товар 4", Price = 200 }
+                    }
+                }
+            };
+
+            foreach (var order in orders)
+            {
+                Console.WriteLine($"Замовлення ID: {order.OrderId}, Дата: {order.OrderDate}");
+                Console.WriteLine("Товари в замовленні:");
+                foreach (var product in order.Products)
+                {
+                    Console.WriteLine($"- {product.Name}, Ціна: {product.Price:C}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
