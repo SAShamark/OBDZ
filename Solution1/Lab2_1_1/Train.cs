d6f3405738f7750553114e396823bfdd3b20a334
sashamark <sashamark7777@gmail.com>
2024-06-20 00:07:13 +0300

    Lab_2_1_1 completed

---
+++ Solution1/Lab2_1_1/Train.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2_1_1
+{
+    internal class Train
+    {
+        public int Id { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int AvailableSeats { get; set; }
+    }
+}
